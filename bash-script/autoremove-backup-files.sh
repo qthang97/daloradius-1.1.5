a1fe8fd704b5eda1af1c07099bf54b65105ba55e
qthang97 <42179581+qthang97@users.noreply.github.com>
2025-12-02 10:03:56 +0700

    Update autoremove-backup-files.sh

--- bash-script/autoremove-backup-files.sh
+++ bash-script/autoremove-backup-files.sh
@@ -19,22 +19,23 @@ if [ ! -d "$PATH_BACKUP" ]; then
   exit 1
 fi
 
-# Đếm file (chỉ file, không tính folder)
+# Count Files (only file)
 NUM_FILES=$(find "$PATH_BACKUP" -maxdepth 1 -type f | wc -l)
 log "$PATH_BACKUP has $NUM_FILES files."
 
-# Nếu số file vượt quá số cần giữ
+# IF num file greate than num file want keep
 if [ "$NUM_FILES" -gt "$NUM_FILE_WANT_KEEP" ]; then
 
   NUM_TO_DELETE=$((NUM_FILES - NUM_FILE_WANT_KEEP))
   log "Need to delete $NUM_TO_DELETE old file(s)."
 
-  # Tìm file cũ nhất, chỉ file, không folder
+  # Find oldest file (only file, skip folder)
   find "$PATH_BACKUP" -maxdepth 1 -type f -printf "%p\n" |
     sort -n |
     head -n "$NUM_TO_DELETE" |
     while read -r filepath; do
 
+      # Remove file
       if rm -f "$filepath"; then
         log "Deleted: $filepath"
       else
